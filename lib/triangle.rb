4116ddb90db911999a95a875901d8b0fbd824951
David Mowrer <david.mowrer@comcast.net>
2014-08-09 13:03:00 -0600

    TDD Shapes/triangle perimeter

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,13 +4,26 @@ require './lib/shape'
 class Triangle < Shape
   attr_reader :base
   attr_reader :height
+  attr_reader :right_angle
 
   def initialize(base, height)
     @base = base
     @height = height
+    right_angle
   end
 
   def area
     (@base * @height) * (0.5)
+  end
+
+  def right_angle
+    @base_sq = @base**2
+    @height_sq = @height**2
+    @right_angle_sq = @base_sq + @height_sq
+    @right_angle = Math.sqrt(@right_angle_sq)
+  end
+
+  def perimeter
+    (@base + @height + @right_angle)
   end
 end
--- spec/lib/triangle_spec.rb
+++ spec/lib/triangle_spec.rb
@@ -87,8 +87,64 @@ describe Triangle do
       let(:base) { 3.25 }
       let(:height) { 6.75 }
       it 'returns the area of the triangle' do
-        expect(subject.area).to be_within(0.01).of(10.97)
+        expect(subject.area).to be_within(0.005).of(10.97)
       end
     end
   end
+
+  describe '#right_angle' do
+    context 'for a triangle with a base of 9 & height of 8'
+    let(:base) { 9 }
+    let(:height) { 8 }
+    it 'returns the length of the right angle' do
+      expect(subject.right_angle).to be_within(0.005).of(12.04)
+    end
+
+    context 'for a triangle with a base of 3 & height of 7' do
+      let(:base) { 3 }
+      let(:height) { 7 }
+      it 'returns the length of the right angle' do
+        expect(subject.right_angle).to be_within(0.005).of(7.62)
+      end
+    end
+
+    context 'for a triangle with a base of 3.25 & height of 6.75' do
+      let(:base) { 3.25 }
+      let(:height) { 6.75 }
+      it 'returns the length of the right angle' do
+        expect(subject.right_angle).to be_within(0.005).of(7.49)
+      end
+    end
+  end
+
+  describe '#perimeter' do
+    context 'a right triangle a base of 9 height of 8 & hypotenuse of 12.04'
+    let(:base) { 9 }
+    let(:height) { 8 }
+    let(:right_angle) { 12.04 }
+    it 'returns the perimeter of a triangle' do
+      expect(subject.perimeter).to be_within(0.005).of(29.04)
+
+    end
+
+    context 'a right triangle a base of 3 height of 7 & hypotenuse of 7.62' do
+      let(:base) { 3 }
+      let(:height) { 7 }
+      let(:right_angle) { 7.62 }
+      it 'returns the perimeter of a triangle' do
+        expect(subject.perimeter).to be_within(0.005).of(17.62)
+      end
+    end
+
+    context 'right triangle base of 3.25 height of 6.75 & hypotenuse of 7.49' do
+      let(:base) { 3.25 }
+      let(:height) { 6.75 }
+      let(:right_angle) { 7.49 }
+      it 'returns the perimeter of a triangle' do
+        expect(subject.perimeter).to be_within(0.005).of(17.49)
+      end
+    end
+
+  end
+
 end
